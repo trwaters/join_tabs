955c93f9474f8675cc1e01f8a8642c52ecb508a6
Tim Waters <waters@lanl.gov>
2019-05-31 14:13:45 -0600

    Added sample dataset

--- join_tabs.sh
+++ join_tabs.sh
@@ -15,11 +15,11 @@
 # the merged tab dumps will appear in dir_name/merged
 
 # Example
-# Given a dir containing output run on 16 procs and 501 dump files 
-# named "TI.block0.out1.00000.tab"..."TI.block15.out1.00000.tab",
+# Included is sample dir containing output run on 4 procs with 10 dumps 
+# named "TI.block0.out1.00000.tab"..."TI.block3.out1.00000.tab",
 # put join_tab++ in the folder containing this dir (call it TI1d)
 # To merge all dumps in TI1d, do 
-# >> ./join_tabs.sh 0 500 TI1d TI
+# >> ./join_tabs.sh 0 9 TI1d TI
 # To merge just the 1st dump in TI1d, do 
 # >> ./join_tabs.sh 0 0 TI1d TI
 # The dir TI1d/merged contains the merged output files
